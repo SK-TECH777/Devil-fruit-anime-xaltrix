65451d8a489b2683f5fbf22c5f056ee3e89bab57
SK-TECH777 <sanjaykingboy9597@gmail.com>
2025-12-15 11:25:06 +0530

    Change message_effect_id to has_spoiler in start.py

--- plugins/start.py
+++ plugins/start.py
@@ -234,7 +234,7 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-	        message_effect_id=5104841245755180586)
+	        has_spoiler = True)
 		
         try: await message.delete()
         except: pass
